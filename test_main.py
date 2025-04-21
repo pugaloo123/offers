50b5f90975a1b4712e59264b2c432d3598aff56d
pugalo0123 <nicglad2005@gmail.com>
2025-04-21 21:14:47 +0700

    Implement fake DB

--- test_main.py
+++ test_main.py
@@ -1,37 +1,29 @@
 from fastapi.testclient import TestClient
 
 from main import app
+from main import db
 
 client = TestClient(app)
+
 
 def test_get_offers():
     response = client.get('/offers')
     assert response.status_code == 200
-    assert response.json() == [
-        {
-           "id": 1,
-           "price": 100,
-           "location": "Томск, пер. Лесной 10",
-           "photos": [
-                "url1",
-                "url2"
-            ],
-           "rooms": 3
-        }
-    ]
+    assert response.json() == db
 
-def test_get_offers():
+
+def test_get_offer():
     response = client.get('/offers/1')
     assert response.status_code == 200
     assert response.json() == {
-       "id": 1,
-       "price": 100,
-       "location": "Томск, пер. Лесной 10",
-       "photos": [
+        "id": 1,
+        "price": 100,
+        "location": "Томск, пер. Лесной 10",
+        "photos": [
             "url1",
             "url2"
         ],
-       "phone": "+7777777777",
-       "total_area": 100,
-       "rooms": 3
+        'phone': '+77777777777',
+        'total_area': 90,
+        "rooms": 3
     }
